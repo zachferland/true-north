d645d59aeab4996e7970d1e0a241ca6bd8b300e5
Zach Ferland <zachferland@gmail.com>
2013-11-10 02:40:09 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Truenorthflow::Application.config.secret_key_base = 'c6a9b31017e9a4dd94816637b91d1eee4de4849e134fc96cc96bd99a320a0c0bd7a3846d38e5568cd44d1f6774bd25add7184087dfa509635aee4a77cd4bd0ab'
